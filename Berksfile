2d5a597ac25b1bd70ff613d56a3fbf1ff82e8119
r_nishio <quick.xp@me.com>
2015-11-17 00:33:43 +0900

    zabbix agent install recipe

--- Berksfile
+++ Berksfile
@@ -1,7 +1,5 @@
 source "http://supermarket.chef.io"
-site :opscode
 
 cookbook 'git'
 cookbook 'mysql',"~> 5.3.6"
 cookbook 'java'
-cookbook 'ark'
--- site-cookbooks/zabbix/recipes/default.rb
+++ site-cookbooks/zabbix/recipes/default.rb
@@ -21,19 +21,4 @@ user "zabbix" do
 end
 
 # Agent Install
-kernel = node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'i386'
-
-ark 'zabbix_agent' do
-  name 'zabbix'
-  url "http://www.zabbix.com/downloads/#{node['zabbix']['agent']['version']}/zabbix_agents_#{node['zabbix']['agent']['version']}.linux2_6.#{kernel}.tar.gz"
-  owner 'zabbix'
-  group 'zabbix'
-  action :put
-  path '/opt'
-  strip_components 0
-  has_binaries ['bin/zabbix_sender', 'bin/zabbix_get', 'sbin/zabbix_agent', 'sbin/zabbix_agentd']
-  notifies :restart, 'service[zabbix_agentd]'
-end
-
-# common directory 作成
-
+package "zabbix-agent"
