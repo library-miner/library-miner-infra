101d5a233df9bbc53dcecd1f73535079cec1f96f
r_nishio <quick.xp@me.com>
2016-01-18 23:50:08 +0900

    zabbix で rubyを使えるように

--- site-cookbooks/zabbix/recipes/default.rb
+++ site-cookbooks/zabbix/recipes/default.rb
@@ -17,8 +17,26 @@ user "zabbix" do
   home '/opt/zabbix'
   shell '/bin/bash'
   group 'zabbix'
+	supports :manage_home => true, :non_unique => false
   system true
 end
+
+directory "/opt/zabbix" do
+	owner 'zabbix'
+	group 'zabbix'
+	mode 00764
+	action :create
+end
+
+
+template ".bashrc" do
+	path "/opt/zabbix" + "/.bashrc"
+	source "bashrc.erb"
+	owner 'zabbix'
+	group 'zabbix'
+	mode 00744
+end
+
 
 # Agent Install
 package "zabbix-agent"
