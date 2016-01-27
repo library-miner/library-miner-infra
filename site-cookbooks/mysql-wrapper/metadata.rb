5af36ecdbfa4e413e92d7ee274bf35508345046c
r_nishio <quick.xp@me.com>
2016-01-27 21:56:12 +0900

    mysql recipe update に伴いwrapper ファイル更新

--- site-cookbooks/mysql-wrapper/metadata.rb
+++ site-cookbooks/mysql-wrapper/metadata.rb
@@ -4,4 +4,5 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures mysql-wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+depends 'mysql', '~> 6.0'
 version          '0.1.0'
--- site-cookbooks/mysql-wrapper/recipes/default.rb
+++ site-cookbooks/mysql-wrapper/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+mysql_service 'default' do
+  port '3306'
+  version '5.6'
+  initial_root_password 'root'
+  action [:create, :start]
+end
